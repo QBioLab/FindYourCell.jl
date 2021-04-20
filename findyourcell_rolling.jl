57641577c4fbeb7ac819eac22f2a1a3271abdf02
H.F <moyuejian@outlook.com>
2021-04-20 16:52:41 +0800

    update rolling

--- findyourcell_rolling.jl
+++ findyourcell_rolling.jl
@@ -1,7 +1,14 @@
 using FileIO
 using TiffImages
 using MAT
+using Dates
+using ProgressMeter
 include("findyourcell.jl")
+"""
+Rolling cell segmentation during capturing
+v0.1.0 hf 04202021
+
+"""
 
 DATA_DIR = "../0417-22-processed-stack-noRball/2021041720-preprocess"
 RET_DIR = "../0417-22-processed-stack-noRball/result"
@@ -11,9 +18,10 @@ unfinished = true
 while unfinished
     for idx in 1:length(stack_list)
         raw_name = stack_list[idx]
-        if time() - stat("$DATA_DIR/$raw_name").mtime > 300 # 300s, be careful 
+        if time() - stat("$DATA_DIR/$raw_name").mtime > 180 # be careful 
             img = try
-                TiffImages.load("$DATA_DIR/$raw_name", mmap=true)
+                load("$DATA_DIR/$raw_name")#, mmap=true)
+				#TiffImages.load("$DATA_DIR/$raw_name")#, mmap=true)
             catch e
                 @warn "Failed to load $raw_name, skip to next stack"
                 continue #TODO: or wait util tiff is free to read
@@ -21,41 +29,56 @@ while unfinished
             mask_name = "$RET_DIR/$(raw_name[1:end-4])_mask.tiff"
             info_name = "$RET_DIR/$(raw_name[1:end-4])_info.mat"
             raw_frame_num = size(img, 3)
-	    	if isfile(info_name)
-	            prev_info = matread(info_name)           
-	            prev_frame_num = length(prev_info["info"])
+	    	if isfile(info_name) #TODO: && time() - stat(info_name).mtime > 400
+	            prev_info = matread(info_name)["info"]           
+	            prev_frame_num = length(prev_info)
             	append_num = raw_frame_num - prev_frame_num
 	            # only process the new coming frames
 	            if append_num > 0
-	                println("Appending $raw_name:$(prev_frame_num+1):$raw_frame_num")
+	                println("Appending $raw_name:$(prev_frame_num+1) $raw_frame_num, $(Dates.now())")
 	                prev_mask = try
 	                    reinterpret(UInt16, TiffImages.load(mask_name))
 	                catch
 	                    @warn "Failed to load mask $mask_name, skip to next stack"
 	                    continue 	                
 					end
-	                mask_patch = Array{UInt16}(undef, size(img)[1:2], append_num)
+	                mask_patch = Array{UInt16}(undef, size(img, 1), size(img,2), append_num)
 	                new_mask = cat(prev_mask, mask_patch, dims=3)
 	                info_patch = Array{Any}(undef, append_num)
 	                new_info = cat(prev_info, info_patch, dims=1)
-	                Threads.@threads for t in prev_frame_num+1:raw_frame_num
-	                    print("*")
+					p = Progress(append_num, 1, "Appending")
+	                @time Threads.@threads for t in prev_frame_num+1:raw_frame_num
+	                    #print("*")
 	                    new_mask[:, :, t], new_info[t] = find_your_cell(img[:, :, t])
+						next!(p)
 	                end
+				else
+					continue
 				end
 			else  # start from 0
-	            println("Processing $raw_name: 1:$raw_frame_num")
+	            println("Processing $raw_name: 1:$raw_frame_num, $(Dates.now())")
     			new_mask = Array{UInt16}(undef, size(img))
     			new_info = Array{Any}(undef, raw_frame_num)
-    			@time @inbounds Threads.@threads for t in 1:raw_frame_num
-        			print("*")
-        			new_mask[:, :, t], new_info[t] = find_your_cell(img[:, :, t])
-    			end
+				slices_num = Threads.nthreads() - 2
+				p = Progress(raw_frame_num, 1, "Processing")
+				@time for t_start in 1:slices_num:raw_frame_num
+					t_end = t_start + slices_num - 1
+					if t_end > raw_frame_num
+						t_end = raw_frame_num
+					end
+    				Threads.@threads for t in t_start:t_end
+        				#print("*")
+        				new_mask[:, :, t], new_info[t] = find_your_cell(img[:, :, t])
+						next!(p)
+    				end
+				end
 			end
-			save(mask_name, new_mask) 
-	        matwrite(info_name, Dict("info"=>new_info ))
-			println("")
-	        println("Done!")
+			println("Writing")
+			#save(mask_name, new_mask) 
+			TiffImages.save(mask_name, reinterpret(Gray{N0f16}, new_mask))
+	        matwrite(info_name, Dict("info"=>new_info))
+	        println("Done! $(Dates.now())")
+			GC.gc()
         end
     end
     sleep(1)   
--- findyourcell_run.jl
+++ findyourcell_run.jl
@@ -1,5 +1,5 @@
 using FileIO
-#using TiffImages
+using TiffImages
 using MAT
 include("findyourcell.jl")
 
@@ -7,17 +7,16 @@ include("findyourcell.jl")
 #data_dir = "../2021032423-preprocess"
 data_dir = "../0417-22-processed-stack-noRball/2021041720-preprocess"
 #ret_dir = ARGS[2]
-ret_dir = "../0417-22-processed-stack-noRbal/result"
+ret_dir = "../0417-22-processed-stack-noRball/result"
 
 file_list = filter(x->occursin(r"G-stack.tiff$", x), readdir( data_dir))
 
-
-for i in parse(Int, ARGS[1]):parse(Int, ARGS[2]) #length(file_list)
 #for i in length(file_list):-1:parse(Int, ARGS[1])
+for i in parse(Int, ARGS[1]):2:parse(Int, ARGS[2]) #length(file_list)
     print(i)
-    img = load("$data_dir/$(file_list[i])")
-    #print("$data_dir/$(file_list[i])")
-    #img = TiffImages.load("$data_dir/$(file_list[i])")
+    #img = load("$data_dir/$(file_list[i])")
+    print("$data_dir/$(file_list[i])")
+    img = TiffImages.load("$data_dir/$(file_list[i])")
     t_len = size(img, 3)
     cell_mask = Array{UInt16}(undef, size(img))
     cell_info = Array{Any}(undef, t_len)
@@ -26,6 +25,6 @@ for i in parse(Int, ARGS[1]):parse(Int, ARGS[2]) #length(file_list)
         cell_mask[:, :, t], cell_info[t] = find_your_cell(img[:, :, t])
     end
     println("")
-    save("$ret_dir/$(file_list[i][1:end-4])_mask.tiff", cell_mask) 
-    matwrite("$ret_dir/$(file_list[i][1:end-4])_info.mat", Dict("info"=>cell_info ))
+    TiffImages.save(mask_name, reinterpret(Gray{N0f16}, new_mask))
+    matwrite("$ret_dir/$(file_list[i][1:end-4])_info.mat", Dict("info"=>cell_info))
 end
